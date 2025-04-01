1779040d46c3a25580e28b05fcfc94ab8de11b45
Mukkun <fitahianalova@gmail.com>
2025-04-01 07:23:14 +0300

    AnnonceController

---
+++ src/Controller/AnnonceController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Annonce;
+use App\Form\AnnonceType;
+use App\Repository\AnnonceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Knp\Component\Pager\PaginatorInterface;
+
+#[Route('/annonce')]
+final class AnnonceController extends AbstractController
+{
+    #[Route(name: 'annonce_index', methods: ['GET'])]
+    public function index(AnnonceRepository $annonceRepository): Response
+    {
+        $query = $annonceRepository->findAll(); 
+        $annonces = $paginator->paginate(
+            $query, 
+            $request->query->getInt('page', 1), // Page actuelle
+            10 // Nombre d'éléments par page
+        );
+
+        return $this->render('annonce/index.html.twig', [
+            'annonces' => $annonces,
+        ]);
+    }
+
+    #[Route('/new', name: 'annonce_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $annonce = new Annonce();
+        $form = $this->createForm(AnnonceType::class, $annonce);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($annonce);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('annonce_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('annonce/new.html.twig', [
+            'annonce' => $annonce,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'annonce_show', methods: ['GET'])]
+    public function show(Annonce $annonce): Response
+    {
+        return $this->render('annonce/show.html.twig', [
+            'annonce' => $annonce,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'annonce_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Annonce $annonce, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(AnnonceType::class, $annonce);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('annonce_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('annonce/edit.html.twig', [
+            'annonce' => $annonce,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'annonce_delete', methods: ['POST'])]
+    public function delete(Request $request, Annonce $annonce, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$annonce->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($annonce);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('annonce_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/AnnonceType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Annonce;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnonceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('prix')
+            ->add('type')
+            ->add('datePublication', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('surface')
+            ->add('ville')
+            ->add('codePostal')
+            ->add('photo')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Annonce::class,
+        ]);
+    }
+}
